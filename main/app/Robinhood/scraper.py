f0afe763084e63474b622785c83cc9c997378b3a
Samarth Desai <samarthdesai1@gmail.com>
2021-02-05 03:45:34 -0500

    added dividend functionality, bugs remain

--- main/app/Robinhood/scraper.py
+++ main/app/Robinhood/scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import datetime
 
 def getRHData():
 
@@ -68,49 +69,142 @@ def getRHData():
       section_transactions = section.find_elements_by_class_name("rh-expandable-item-a32bb9ad")
       transactions.extend(section_transactions)
 
-
   #transactions = driver.find_elements_by_class_name("rh-expandable-item-a32bb9ad")
 
   transaction_types = {
-      "SELL": "Sell",
-      "BUY": "Buy",
+      "MARKET_SELL": "Market Sell",
+      "MARKET_BUY": "Market Buy",
+      "LIMIT_SELL": "Limit Sell",
+      "LIMIT_BUY": "Limit Buy",
       "DIVIDEND": "Dividend",
       "CANCELED": "Canceled"
   }
 
   transaction_arr = []
+  company_name_dict = {}
+
+  transactions_with_dates = []
 
   for transaction in transactions:
 
     header_text = transaction.find_element_by_xpath(".//div[@class='_2VPzNpwfga_8Mcn-DCUwug']").text
 
+    if transaction_types["LIMIT_SELL"] in header_text or transaction_types["LIMIT_BUY"] in header_text or transaction_types["MARKET_SELL"] in header_text or transaction_types["MARKET_BUY"] in header_text:
+
+      info_children = transaction.find_elements_by_xpath(".//div[@class='css-1qd1r5f']")
+
+      date_node = info_children[7]
+      date = date_node.get_attribute('textContent')
+
+      transaction_with_date = (transaction, date)
+      transactions_with_dates.append(transaction_with_date)
+
+    elif transaction_types["DIVIDEND"] in header_text
+
+      date_node = transaction.find_elements_by_xpath(".//span[@class='css-zy0xqa']")[1]
+      date = transaction_date_node.get_attribute('textContent')
+
+      transaction_with_date = (transaction, date)
+      transactions_with_dates.append(transaction_with_date)
+
+  sorted_transactions = transactions_with_dates.sort(key = lambda date: datetime.strptime(date[1], '%b %d, %Y')) 
+
+  for transaction_number in range(len(sorted_transactions)-1, -1, -1):
+
+    transaction = sorted_transactions[transaction_number][0]
+
+    header_text = transaction.find_element_by_xpath(".//div[@class='_2VPzNpwfga_8Mcn-DCUwug']").text
+
     canceled_text = transaction.find_element_by_xpath(".//div[@class='_22YwnO0XVSevsIC6rD9HS3']").text
+
+    company_name_title = header_text.split("\n")
+
+    company_name_list = company_name_title[0].split(" ")
+
+    print(company_name_list)
     
     if transaction_types["CANCELED"] in canceled_text:
       continue
 
-    if transaction_types["SELL"] in header_text or transaction_types["BUY"] in header_text:
+    if transaction_types["LIMIT_SELL"] in header_text or transaction_types["LIMIT_BUY"] in header_text:
 
-          info_children = transaction.find_elements_by_xpath(".//div[@class='css-1qd1r5f']")
-          ticker_symbol_node = info_children[1]
-          ticker_symbol = ticker_symbol_node.find_element_by_xpath(".//a").get_attribute('textContent')
-          transaction_type_node = info_children[3]
-          transaction_type = transaction_type_node.find_element_by_xpath(".//span").get_attribute('textContent')
-          transaction_type_split = transaction_type.split(" ")
-          transaction_type_final = transaction_type_split[1]
-          transaction_date_node = info_children[7]
-          transaction_date = transaction_date_node.get_attribute('textContent')
-          filled_transaction_node = info_children[15]
-          filled_transaction = filled_transaction_node.get_attribute('textContent')
-          filled_transaction_split = filled_transaction.split(" ")
-          quantity = filled_transaction_split[0]
-          price = filled_transaction_split[3]
-          price = price.split("$")[1]
-          total_value_node = info_children[17]
-          total_value = total_value_node.get_attribute('textContent')
-          transactionTuple = (ticker_symbol, transaction_type_final, quantity, price, transaction_date)
-          transaction_arr.insert(0, transactionTuple)
+      company_name = " ".join(company_name_list[:-2])
+
+      info_children = transaction.find_elements_by_xpath(".//div[@class='css-1qd1r5f']")
+      ticker_symbol_node = info_children[1]
+      ticker_symbol = ticker_symbol_node.find_element_by_xpath(".//a").get_attribute('textContent')
+
+      if company_name not in company_name_dict.keys():
+        company_name_dict[company_name] = ticker_symbol
       
+      transaction_type_node = info_children[3]
+      transaction_type = transaction_type_node.find_element_by_xpath(".//span").get_attribute('textContent')
+      transaction_type_split = transaction_type.split(" ")
+      transaction_type_final = transaction_type_split[1]
+      transaction_date_node = info_children[7]
+      transaction_date = transaction_date_node.get_attribute('textContent')
+      filled_transaction_node = info_children[17]
+      filled_transaction = filled_transaction_node.get_attribute('textContent')
+      filled_transaction_split = filled_transaction.split(" ")
+      quantity = filled_transaction_split[0]
+      price = filled_transaction_split[3]
+      price = price.split("$")[1]
+      total_value_node = info_children[19]
+      total_value = total_value_node.get_attribute('textContent')
+      transaction_tuple = (ticker_symbol, transaction_type_final, quantity, price, total_value, transaction_date)
+      transaction_arr.insert(0, transaction_tuple)
+      continue
+
+    if transaction_types["MARKET_SELL"] in header_text or transaction_types["MARKET_BUY"] in header_text:
+
+      company_name = " ".join(company_name_list[:-2])
+
+      info_children = transaction.find_elements_by_xpath(".//div[@class='css-1qd1r5f']")
+      ticker_symbol_node = info_children[1]
+      ticker_symbol = ticker_symbol_node.find_element_by_xpath(".//a").get_attribute('textContent')
+
+      if company_name not in company_name_dict.keys():
+        company_name_dict[company_name] = ticker_symbol
+
+      transaction_type_node = info_children[3]
+      transaction_type = transaction_type_node.find_element_by_xpath(".//span").get_attribute('textContent')
+      transaction_type_split = transaction_type.split(" ")
+      transaction_type_final = transaction_type_split[1]
+      transaction_date_node = info_children[7]
+      transaction_date = transaction_date_node.get_attribute('textContent')
+      filled_transaction_node = info_children[15]
+      filled_transaction = filled_transaction_node.get_attribute('textContent')
+      filled_transaction_split = filled_transaction.split(" ")
+      quantity = filled_transaction_split[0]
+      price = filled_transaction_split[3]
+      price = price.split("$")[1]
+      total_value_node = info_children[17]
+      total_value = total_value_node.get_attribute('textContent')
+      transaction_tuple = (ticker_symbol, transaction_type_final, quantity, price, total_value, transaction_date)
+      transaction_arr.insert(0, transaction_tuple)
+      continue
+
+    if transaction_types["DIVIDEND"] in header_text:
+
+      company_name = " ".join(company_name_list[2:])
+
+      ticker_symbol = company_name_dict[company_name]
+
+      info_children = transaction.find_elements_by_xpath(".//div[@class='_2SYphfY1DF71e5bReqgDyP']")
+
+      transaction_type = "Dividend"
+      transaction_date_node = transaction.find_elements_by_xpath(".//span[@class='css-zy0xqa']")[1]
+      transaction_date = transaction_date_node.get_attribute('textContent')
+
+      quantity = info_children[0].get_attribute('textContent')
+
+      price = info_children[1].get_attribute('textContent')
+
+      total_value = info_children[2].get_attribute('textContent')
+
+      transaction_tuple = (ticker_symbol, transaction_type, quantity, price, total_value, transaction_date)
+      transaction_arr.insert(0, transaction_tuple)
+
   return transaction_arr
 
 
