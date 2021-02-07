c4869e13bcef4e0e6f85fd3c3a4d582e57c44d88
ham <hamsa.v.pillai@gmail.com>
2021-02-07 01:54:57 -0600

    Fixed current stock price

--- main/main.py
+++ main/main.py
@@ -14,6 +14,17 @@ for stock in transactions.keys():
   print(f"{stock} \n realized gains: {arr[0]} \n remaining shares: {arr[1]} \n unrealized gains: {arr[2]}")
 '''
 
+def getCurrentPrice(ticker):
+  stock_current_price = float(0)
+  try:
+    stock_current_info = yf.Ticker(ticker).info
+    stock_current_price = (float(stock_current_info['ask']) + float(stock_current_info['ask']))/2
+    if (float(stock_current_info['ask']) == float(0) or float(stock_current_info['buy']) == float(0)):
+      stock_current_price = float(stock_current_info['open'])
+  except:
+    pass
+  return stock_current_price
+
 transactions_store = ts.TransactionStore()
 transactions_arr = scraper.getRHData()
 
@@ -23,20 +34,17 @@ for transaction in transactions_arr:
 
 index = 0
 
+total_gains = float(0)
 for stock in transactions_store.transactions_dict.keys():
 
   #arr = gains.getGains(transactions_store.transactions_dict[stock], 1000, method="AVERAGE_COST")
   #print(f"{stock} \n realized gains: {arr[0]} \n remaining shares: {arr[1]} \n unrealized gains: {arr[3]}")
-  stock_current_price = ""
-  try:
-    stock_current_info = yf.Ticker(stock).info
-    stock_current_price = stock_current_info['ask']
-  except:
-    stock_current_price = 0
+  stock_current_price = getCurrentPrice(stock)
   arr = gains.getGains(transactions_store.transactions_dict[stock], stock_current_price)
+  total_gains+= (float(arr[0]) + float(arr[2]))
   print(f"{stock} \n realized gains: {arr[0]} \n remaining shares: {arr[1]} \n unrealized gains: {arr[2]}")
 
-
+print("Total gains: ", total_gains)
 '''
 for i in range(50):
   print("\n")
@@ -52,6 +60,7 @@ for stock in transactions_store.transactions_dict.keys():
   arr = gains.getGains(transactions_store.transactions_dict[stock], 1000)
   print(f"{stock} \n realized gains: {arr[0]} \n remaining shares: {arr[1]} \n unrealized gains: {arr[2]}")
 '''
+
 
 
 
